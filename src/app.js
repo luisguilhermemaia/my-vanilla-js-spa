500074042a95bed30e57660d78096c49ae7a76c1
Luis Guilherme <maialuisguilherme@gmail.com>
2019-02-17 16:08:33 -0300

    Register page

--- src/app.js
+++ src/app.js
@@ -3,28 +3,30 @@
 import Error404 from './views/pages/Error404.js';
 import Posts from './views/pages/Posts.js';
 import Login from './views/pages/Login.js';
+import Register from './views/pages/Register.js';
 
 import Navbar from './views/components/Navbar.js';
 
 const routes = {
   '/': Login,
+  '/register': Register,
   '/posts': Posts
 };
 
 const router = async () => {
   const header = null || document.getElementById('header-container');
-  const content = null || document.getElementById('page-container');
+  const content = null || document.getElementById('content-container');
 
   header.innerHTML = await Navbar.render();
   await Navbar.after_render();
 
-  let request = location.hash
+  const request = location.hash
     .slice(1)
     .toLowerCase()
     .split('/')[1];
-  let parsedURL = request ? '/' + request : '/';
+  const parsedURL = request ? '/' + request : '/';
 
-  let page = routes[parsedURL] ? routes[parsedURL] : Error404;
+  const page = routes[parsedURL] ? routes[parsedURL] : Error404;
   content.innerHTML = await page.render();
   await page.after_render();
 };
---
+++ src/views/pages/Register.js
@@ -0,0 +1,126 @@
+const Register = {
+  render: async () => {
+    return /*html*/ `
+      <form>
+        <label>
+          <img
+            class="logo"
+            src="/user.svg"
+            alt="user"
+          />
+          <input id="file-chooser" type="file" class="displayNone" />
+        </label>
+        <div class="form-body">
+          <input
+            name="name"
+            id="name"
+            type="text"
+            placeholder="Enter your name"
+          />
+          <div class="error-message displayNone" id="e-error-name"></div>
+          <input
+            name="email"
+            id="email"
+            type="text"
+            placeholder="Enter your email"
+          />
+          <div class="error-message displayNone" id="e-error-email"></div>
+          <input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <div class="error-message displayNone" id="e-error-password"></div>
+          <input 
+            name="date" 
+            id="date" 
+            type="date" 
+            placeholder="Enter your birth date"
+            />
+          <div class="error-message displayNone" id="e-error-date">Required</div>
+          <button type="button" id="e-login-button">Submit</button>
+        </div>
+        <p>
+          Don't have account?
+          <a href="/#/">Sign up here.</a>
+        </p>
+      </form>
+    `;
+  },
+  after_render: async () => {
+    document
+      .getElementById('e-login-button')
+      .addEventListener('click', async () => {
+        const formValues = {
+          name: document.getElementById('name').value,
+          email: document.getElementById('email').value,
+          password: document.getElementById('password').value,
+          date: document.getElementById('date').value
+        };
+
+        if (validateForm(formValues)) {
+          var userExists = await handleSubmit(formValues);
+          if (userExists) {
+            window.location.hash = '/posts';
+          }
+        }
+      });
+  }
+};
+
+const validateForm = formValues => {
+  let formIsInvalid = true;
+  Object.keys(formValues).forEach(function(key) {
+    const error = document.getElementById('e-error-' + key);
+    if (formValues[key] === '') {
+      error.classList.remove('displayNone');
+      error.textContent = key + ' is required';
+      formIsInvalid = false;
+    } else {
+      error.classList.add('displayNone');
+    }
+  });
+
+  const passwordError = document.getElementById('e-error-password');
+  if (formValues.password.length < 4) {
+    error.classList.remove('displayNone');
+    error.textContent = 'Password must be at min 4 characters';
+    formIsInvalid = false;
+  } else {
+    passwordError.classList.add('displayNone');
+  }
+
+  const emailError = document.getElementById('e-error-email');
+  if (formValues.email !== '' && !emailIsValid(formValues.email)) {
+    error.classList.remove('displayNone');
+    error.textContent = 'Please enter a valid email';
+    formIsInvalid = false;
+  } else {
+    emailError.classList.add('displayNone');
+  }
+
+  return formIsInvalid;
+};
+
+const emailIsValid = email => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const handleSubmit = async values => {
+  try {
+    var response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    });
+    return await response.json();
+  } catch (error) {
+    console.warn(error);
+  }
+};
+
+export default Register;
